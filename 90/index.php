955c667af91f4aa7e71d65e66d12c218f4a00d83
Pavel Demyanov <demyanovpaul@yandex.ru>
2021-08-15 11:44:15 +0300

    Add new extension №90 spl_autoload_register

---
+++ 90/index.php
@@ -0,0 +1,6 @@
+<?php
+    spl_autoload_register();
+
+    var_dump(new Core\Admin\Controller);
+    var_dump(new Core\User);
+    var_dump(new Project\User\Data);
---
+++ 90/project/user/data.php
@@ -0,0 +1,7 @@
+<?php
+    namespace Project\User\Data;
+
+    class Data 
+    {
+        
+    }
