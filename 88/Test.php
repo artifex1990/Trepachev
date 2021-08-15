bfd1dc965bc10a338e7be903d945dc7611929403
Pavel Demyanov <demyanovpaul@yandex.ru>
2021-08-15 11:17:50 +0300

    Add new extension №88 create use

---
+++ 88/Test.php
@@ -0,0 +1,14 @@
+<?php
+	namespace Project;
+    use \Core\Users\Data;
+	
+	class Test
+	{
+		public function __construct()
+		{
+			// Создаем 3 объекта одного класса:
+			$data1  = new Data('user1');
+			$data2  = new Data('user3');
+			$data3  = new Data('user3');
+		}
+	}
---
+++ 88/Test2.php
@@ -0,0 +1,13 @@
+<?php
+	namespace Project;
+    use \Core\Admin\Model;
+    use \Core\Users\Storage\Data;
+	
+	class Test2
+	{
+		public function __construct()
+		{
+			$model = new Model;
+			$data  = new Data;
+		}
+	}
