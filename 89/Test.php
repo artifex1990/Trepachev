d5099de5239a12e52d661a7f08be547250df2aed
Pavel Demyanov <demyanovpaul@yandex.ru>
2021-08-15 11:23:42 +0300

    Add new extension №89 create use Page

---
+++ 89/Test.php
@@ -0,0 +1,13 @@
+<?php
+	namespace Project;
+	use \Resource\Controller\Page as ControllerPage;
+	use \Resource\Model\Page as ModelPage;
+	
+	class Test
+	{
+		public function __construct()
+		{
+			$pageController  = new ControllerPage;
+			$pageModel       = new ModelPage;
+		}
+	}
