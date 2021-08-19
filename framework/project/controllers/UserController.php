ac839aad3d8f477aa11d9dd316e34a0cd091bda6
stokato <demyanovpaul@yandex.ru>
2021-08-20 00:13:35 +0300

    Add new extension №94.2 is work UserController

---
+++ framework/project/controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+    namespace Project\Controller;
+    use \Core\Controller;
+
+    class UserController extends Controller
+    {
+        private $users;
+
+        public function __constructor()
+        {
+            $this->users = [
+                1 => ['name'=>'user1', 'age'=>'23', 'salary' => 1000],
+                2 => ['name'=>'user2', 'age'=>'24', 'salary' => 2000],
+                3 => ['name'=>'user3', 'age'=>'25', 'salary' => 3000],
+                4 => ['name'=>'user4', 'age'=>'26', 'salary' => 4000],
+                5 => ['name'=>'user5', 'age'=>'27', 'salary' => 5000],
+            ];
+        }
+    }
