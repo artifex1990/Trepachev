026d871ba8c414af131dda173803f51504efa540
stokato <demyanovpaul@yandex.ru>
2021-08-21 02:36:16 +0300

    Add new extension №95 create viewer page

--- framework/project/config/routes.php
+++ framework/project/config/routes.php
@@ -2,17 +2,6 @@
 	use \Core\Route;
 	
 	return [
-		new Route('/my-page1/', 'page', 'show1'), 
-		new Route('/my-page2/', 'page', 'show2'), 
-		new Route('/my-test1/', 'test', 'act1'), 
-		new Route('/my-test2/', 'test', 'act2'), 
-		new Route('/my-test3/', 'test', 'act3'), 
-		new Route('/my-test/:var1/:var2/', 'test', 'act'), 
-		new Route('/nums/:n1/:n2/:n3/', 'num', 'sum'),
-		new Route('/user/all/', 'user', 'all'),
-		new Route('/user/first/:n', 'user', 'first'),
-		new Route('/user/:id/', 'user', 'show'),
-		new Route('/user/:id/:key/', 'user', 'info'),
-		
+		new Route('/my-page/', 'page', 'act'), 
 	];
 	
--- framework/project/controllers/PageController.php
+++ framework/project/controllers/PageController.php
@@ -4,13 +4,11 @@
 
     class PageController extends Controller
     {
-        public function show1()
-        {
-            echo '1';
-        }
-
-        public function show2()
-        {
-            echo '2';
-        }
+        public function act()
+		{
+			return $this->render('page/act', [
+				'header' => 'список юзеров',
+				'users'  => ['user1', 'user2', 'user3'],
+			]);
+		}
     }
